49178ca07121b37f4f355e9fc29da1695967ff03
Vinithk23 <vinithk23@gmail.com>
2022-09-16 16:59:39 +0530

    Header restriction applied

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "./main";
-import "./owl.carousel.min";
-import "./aos";
-
 import "@hotwired/turbo-rails"
 import "controllers"
--- app/javascript/main.js
+++ app/javascript/main.js
@@ -35,112 +35,108 @@
 	  console.log('show');
 	});
 
-  // aos
-  AOS.init({
-    duration: 1000
-  });
 
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
-    autoplay: true,
-    margin:10,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
-    autoplayHoverPause: true,
-    items: 1,
-    autoheight: true,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:1,
-        nav:false
-      },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-	});
+	// // home slider
+	// $('.home-slider').owlCarousel({
+  //   loop:true,
+  //   autoplay: true,
+  //   margin:10,
+  //   animateOut: 'fadeOut',
+  //   animateIn: 'fadeIn',
+  //   nav:true,
+  //   autoplayHoverPause: true,
+  //   items: 1,
+  //   autoheight: true,
+  //   navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+  //   responsive:{
+  //     0:{
+  //       items:1,
+  //       nav:false
+  //     },
+  //     600:{
+  //       items:1,
+  //       nav:false
+  //     },
+  //     1000:{
+  //       items:1,
+  //       nav:true
+  //     }
+  //   }
+	// });
 
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:2,
-        nav:false
-      },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+	// // owl carousel
+	// var majorCarousel = $('.js-carousel-1');
+	// majorCarousel.owlCarousel({
+  //   loop:true,
+  //   autoplay: true,
+  //   stagePadding: 7,
+  //   margin: 20,
+  //   animateOut: 'fadeOut',
+  //   animateIn: 'fadeIn',
+  //   nav: true,
+  //   autoplayHoverPause: true,
+  //   items: 3,
+  //   navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+  //   responsive:{
+  //     0:{
+  //       items:1,
+  //       nav:false
+  //     },
+  //     600:{
+  //       items:2,
+  //       nav:false
+  //     },
+  //     1000:{
+  //       items:3,
+  //       nav:true,
+  //       loop:false
+  //     }
+  // 	}
+	// });
 
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    // animateOut: 'fadeOut',
-    // animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    autoHeight: true,
-    items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:2,
-        nav:false
-      },
-      1000:{
-        items:3,
-        dots: true,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+	// // owl carousel
+	// var major2Carousel = $('.js-carousel-2');
+	// major2Carousel.owlCarousel({
+  //   loop:true,
+  //   autoplay: true,
+  //   stagePadding: 7,
+  //   margin: 20,
+  //   // animateOut: 'fadeOut',
+  //   // animateIn: 'fadeIn',
+  //   nav: true,
+  //   autoplayHoverPause: true,
+  //   autoHeight: true,
+  //   items: 3,
+  //   navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+  //   responsive:{
+  //     0:{
+  //       items:1,
+  //       nav:false
+  //     },
+  //     600:{
+  //       items:2,
+  //       nav:false
+  //     },
+  //     1000:{
+  //       items:3,
+  //       dots: true,
+  //       nav:true,
+  //       loop:false
+  //     }
+  // 	}
+	// });
 
-  var siteStellar = function() {
-    $(window).stellar({
-      responsive: false,
-      parallaxBackgrounds: true,
-      parallaxElements: true,
-      horizontalScrolling: false,
-      hideDistantElements: false,
-      scrollProperty: 'scroll'
-    });
-  }
-  siteStellar();
+  // var siteStellar = function() {
+  //   $(window).stellar({
+  //     responsive: false,
+  //     parallaxBackgrounds: true,
+  //     parallaxElements: true,
+  //     horizontalScrolling: false,
+  //     hideDistantElements: false,
+  //     scrollProperty: 'scroll'
+  //   });
+  // }
+  // siteStellar();
 
   var smoothScroll = function() {
     var $root = $('html, body');
@@ -154,18 +150,18 @@
   }
   smoothScroll();
 
-  var dateAndTime = function() {
-    $('#m_date').datepicker({
-      'format': 'm/d/yyyy',
-      'autoclose': true
-    });
-    $('#checkin_date, #checkout_date').datepicker({
-      'format': 'd MM, yyyy',
-      'autoclose': true
-    });
-    $('#m_time').timepicker();
-  };
-  dateAndTime();
+  // var dateAndTime = function() {
+  //   $('#m_date').datepicker({
+  //     'format': 'm/d/yyyy',
+  //     'autoclose': true
+  //   });
+  //   $('#checkin_date, #checkout_date').datepicker({
+  //     'format': 'd MM, yyyy',
+  //     'autoclose': true
+  //   });
+  //   $('#m_time').timepicker();
+  // };
+  // dateAndTime();
 
 
   var windowScroll = function() {
