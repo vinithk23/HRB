25a1812ba4304774804b93b55d30659336602d74
Vinithk23 <vinithk23@gmail.com>
2022-09-16 13:37:40 +0530

    devise register and login completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  } 
   resources :features
-  devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
