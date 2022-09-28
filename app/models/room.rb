b764f06092e444ba8123cbec8065b75ab063a7e4
vinithk23 <vinithk23@gmail.com>
2022-09-28 17:24:34 +0530

    Room status enum Updated

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
     belongs_to :category
     has_many :booking
-    enum :state, [:Available, :Booked, :Blocked]
+    enum :state, [:Available, :Blocked]
     validates :room_no, presence:true, uniqueness: true, numericality: { only_integer: true, greater_than: 1, less_than: 10000 }
     validates :status, acceptance: { accept: Room.states.keys.to_a }
 end
