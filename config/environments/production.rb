befad9f80f3f321e261358577500a0f571f577a1
vinithk23 <vinithk23@gmail.com>
2022-10-03 19:55:00 +0530

    config.host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.hosts << "enigmatic-ocean-51639.herokuapp.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
