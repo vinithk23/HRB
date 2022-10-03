01dd3aaa0e18881dd307b869e3c6dad37b41d5a2
vinithk23 <vinithk23@gmail.com>
2022-10-03 19:47:16 +0530

    redis pwd

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,9 +9,10 @@ Sidekiq.configure_server do |config|
     Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_scheduler.yml', __FILE__))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
-  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', namespace: "#{Rails.env}" }
+
+  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: 6yedgGot7w0BkxdQTM1wxZYPtIwVbpdA, namespace: "#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', namespace: "#{Rails.env}" }
+  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: 6yedgGot7w0BkxdQTM1wxZYPtIwVbpdA, namespace: "#{Rails.env}" }
 end
