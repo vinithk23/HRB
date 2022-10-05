753d6c5153270f8cbdada494f1d93cc28b063acf
vinithk23 <vinithk23@gmail.com>
2022-10-05 12:16:26 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require "sidekiq/web"
 Rails.application.routes.draw do
+  default_url_options :host => "enigmatic-ocean-51639.herokuapp.com"
   Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
     username == ENV["SIDEKIQ_USER_NAME"] && password == ENV["SIDEKIQ_PASSWORD"]
   end
