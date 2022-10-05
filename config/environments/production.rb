94a8aabcd39001a9d838105ba05aa2417d779f65
vinithk23 <vinithk23@gmail.com>
2022-10-05 12:09:11 +0530

    Rails.application.routes.default_url_options[:host] = 'enigmatic-ocean-51639.herokuapp.com'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   
   config.hosts << "enigmatic-ocean-51639.herokuapp.com"
+  Rails.application.routes.default_url_options[:host] = 'enigmatic-ocean-51639.herokuapp.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
