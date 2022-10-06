dcba8a77bc7824264f81fd5bd53e17ebe7e289dc
vinithk23 <vinithk23@gmail.com>
2022-10-06 13:38:04 +0530

    env credential procedure fixed and send_mail enhanced

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_API_KEY'],
+    password: Rails.application.credentials.dig(:sendgrid_api_key),
     domain: ENV['SENDGRID_MAIL_DOMAIN'],
     address: ENV['SENDGRID_ADDRESS'],
     port: 587,
