47f9a0e810a0a21fe0aae11b6162e2a991ac40c4
vinithk23 <vinithk23@gmail.com>
2022-10-07 14:21:39 +0530

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,12 +10,12 @@ Sidekiq.configure_server do |config|
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 
-  # config.redis = { url: 'redis://localhost:6379/0', namespace: "#{Rails.env}" }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "#{Rails.env}" }
 
-  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: Rails.application.credentials.dig(:redis_password), namespace: "#{Rails.env}" }
+  # config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: Rails.application.credentials.dig(:redis_password), namespace: "#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  # config.redis = { url: 'redis://localhost:6379/0', namespace: "#{Rails.env}" }
-  config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: Rails.application.credentials.dig(:redis_password), namespace: "#{Rails.env}" }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "#{Rails.env}" }
+  # config.redis = { url: 'redis://redis-11627.c61.us-east-1-3.ec2.cloud.redislabs.com:11627', password: Rails.application.credentials.dig(:redis_password), namespace: "#{Rails.env}" }
 end
